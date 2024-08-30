999c422d573c7d3285f5a53f97aa33ab3cc7a04e
Aakash Gaur <aakash.sunrise23@gmail.com>
2024-08-31 01:17:32 +0530

    Update jwtUtils.js

--- Backend/utils/jwtUtils.js
+++ Backend/utils/jwtUtils.js
@@ -1,4 +1,3 @@
-// utils/jwtUtils.js
 const jwt = require("jsonwebtoken");
 const JWT_SECRET =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
